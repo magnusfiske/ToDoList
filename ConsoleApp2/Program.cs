b9d32411021c9aa98c8601bee2e6445d18f44ede
Magnus <Magnus@MAGNUS>
2022-04-05 13:53:12 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Listan2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            Tdl ts = new Tdl();
+
+            Console.WriteLine("Att göra-listan\n--------\n1. Skapa ny lista       2. Ladda sparad lista\n");
+            bool stay = true;
+            do
+            {
+                string input = Console.ReadLine();
+                
+                switch (input)
+                {
+                    case "1":
+                        stay = false;
+                        ts.ListMeny(lista);
+                        break;
+                    case "2":
+                        stay = false;
+                        ts.Ladda();
+                        ts.ListMeny(lista);
+                        break;
+                    default:
+                        stay = true;
+                        Console.WriteLine("Välj '1' eller '2', pappskalle!");
+                        break;
+                } 
+            } while (stay == true);
+        }
+    }
+}
+
+
---
+++ ConsoleApp2/Tdl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listan2
+{
+    class Tdl
+    {
+     
+
+        public Tdl()
+        {
+            
+        }
+
+        //Metoder
+        public void ListMeny(List<string> ts)
+        {
+            bool stay = true;
+            do
+            {
+                Console.WriteLine("1. Lägg till         2. Spara\n3. Avsluta");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        AddItem(ts);
+                        Print(ts);
+                        break;
+                    case "2":
+                        SaveList(ts);
+                        break;
+                    case "3":
+                        stay = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ange '1', '2' eller '3', dumskalle!");
+                        break;
+                }
+            } while (stay == true);
+
+
+        }
+
+        public static void AddItem(List<string> ts)
+        {
+            Console.Write("Lägg till något till listan: ");
+            string input = Console.ReadLine();
+            ts.Add(input);
+        }
+
+        public static void Print(List<string> ts)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Att göra-listan \n___________");
+            for (int i = 0; i < ts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {ts[i]}");
+            }
+            Console.WriteLine();
+        }
+        public static void SaveList(List<string> ts)
+        {
+            using StreamWriter spara = new StreamWriter("sparadLista.txt");
+            { // Skriv in alla punkter i listan:
+                foreach (string i in ts)
+                {
+                    spara.WriteLine(i);
+                }
+                spara.Close();
+            }
+            Console.WriteLine("Listan sparad.");
+        }
+
+        public void Ladda()
+        {     
+            List<string> ts = File.ReadAllLines("sparadLista.txt")
+            .Select(line => line.Trim())
+            .ToList();
+            Console.WriteLine();
+            Print(ts);
+        }
+    }
+}
